fecbcd546d6a830a93beedba3e756f3ac54cff1c
gabrielconte14 <gabrielconte14@gmail.com>
2018-10-20 11:07:10 -0300

    Created Xcode project

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Gabriel Conte on 20/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
